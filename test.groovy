c0ccd89b51319746bfe4776ada4b9f551b133f4a
Indrayan123 <indrayan.chakraborty@gmail.com>
2017-09-14 23:59:45 -0500

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,9 +1,8 @@
 node{
-  //stage('Invoke Local Script'){
-  def print_details=load("./Print_Details.groovy")
-script{
-purpose='SAmple test'
 
-print_details(purpose)
-}}
-//}
+  def print_details = load("Print_Details.groovy")
+
+
+
+print_details("SAmple test")
+}
